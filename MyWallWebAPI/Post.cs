6aa17c80c815c2fb843eee7b9ff8b740c1d56f9d
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-10-06 22:56:17 -0400

    class Post

--- MyWallWebAPI/Post.cs
+++ MyWallWebAPI/Post.cs
@@ -1,6 +1,7 @@
 ﻿namespace MyWallWebAPI {
-    public class Post 
+    public class Post //Entidade POST
     {
+        //Atributos da entidade
         public int Id { get; set; }
         public string Titulo { get; set; }
         public string Conteudo { get; set; }
