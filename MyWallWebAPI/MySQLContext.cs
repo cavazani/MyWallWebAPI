56e318d474a616aa8202399cf26932f89ba6589d
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-10-10 22:32:58 -0400

    DbContext

--- MyWallWebAPI/MySQLContext.cs
+++ MyWallWebAPI/MySQLContext.cs
@@ -4,6 +4,16 @@ namespace MyWallWebAPI {
 
     public class MySQLContext : DbContext
     {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) 
+        { 
+        }
 
+        //Entidade Post com nome da variavel de Post
+        public DbSet<Post> Post { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<Post>();
+        }
     }
 }
