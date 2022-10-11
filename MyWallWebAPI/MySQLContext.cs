e4053b0ee634a65d3137304670aae51ade986f42
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-10-11 17:44:10 -0400

    migration

--- MyWallWebAPI/MySQLContext.cs
+++ MyWallWebAPI/MySQLContext.cs
@@ -1,18 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MyWallWebAPI {
-
-    public class MySQLContext : DbContext
-    {
-        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) 
-        { 
+    public class MySQLContext : DbContext {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {
         }
-
         //Entidade Post com nome da variavel de Post
         public DbSet<Post> Post { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder) 
-        {
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.Entity<Post>();
         }
     }
